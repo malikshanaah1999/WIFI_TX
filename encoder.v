22da2e4d9ff584012a02bb6cdb6e3fe1bd5fa5c5
Malik Shanaah <95984769+malikshanaah1999@users.noreply.github.com>
2023-03-19 13:20:44 +0200

    Update encoder.v

--- encoder.v
+++ encoder.v
@@ -1,15 +1,15 @@
 //We work with 1/2 Code rate. 
 
-module encoder(input ready, input clk, input rst, input in_bit, output [1:0] x_encoded);
+module encoder(input clk, input rst, input in_bit, output [1:0] x_encoded);
 
 reg [6:0] state;
 
-if (ready) begin
+
     //Odd bit
     assign x_encoded[1] = in_bit ^ state[6] ^ state[5] ^ state[3] ^ state[1];
     //Even bit
     assign x_encoded[0] = in_bit ^ state[1] ^ state[4] ^ state[3] ^ state[2];
-end
+
 always @ (posedge clk, negedge rst) begin
 	if(~rst) begin
 		state <= 7'd0;
